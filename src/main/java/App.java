914b2c6d627dfbf92bf49e018c918c66d67057d1
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2024-02-14 14:54:15 +0530

    restassured execution

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public Map<String, String> demo() {
+        String baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
+        RestAssured.baseURI = baseURI;
+        Response response = RestAssured.given().get().then().extract().response();
+        response.prettyPrint();
+
+        //     return [base_url, method, params, headers, request body, response_body, status code]
+        Map<String, String> x = new HashMap<>();
+        x.put("baseURL", baseURI);
+        x.put("method", "get");
+        x.put("parameters", null);
+        x.put("headers", null);
+        x.put("payload", null);
+        x.put("response", response.getBody().toString());
+        x.put("status-code", String.valueOf(response.statusCode()));
+        return x;
+    }
+}
