defc7f2da0a8079da1dd3b756a97e413f680eecb
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2024-02-19 13:36:26 +0530

    workflow updates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,7 @@ public class App {
         RestAssured.baseURI = baseURI;
         Response response = RestAssured.given().get().then().extract().response();
         response.prettyPrint();
+
         //     return [base_url, method, params, headers, request body, response_body, status code]
         Map<String, String> x = new HashMap<>();
         x.put("baseURL", baseURI);
@@ -17,7 +18,7 @@ public class App {
         x.put("parameters", null);
         x.put("headers", null);
         x.put("payload", null);
-        x.put("response", response.getBody().toString());
+        x.put("response", response.asString());
         x.put("status-code", String.valueOf(response.statusCode()));
         return x;
     }
