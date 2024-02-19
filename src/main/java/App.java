486e121d8d9e0a52992dc7e7714e56281a60ed94
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2024-02-19 13:27:37 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,6 @@ public class App {
         RestAssured.baseURI = baseURI;
         Response response = RestAssured.given().get().then().extract().response();
         response.prettyPrint();
-
         //     return [base_url, method, params, headers, request body, response_body, status code]
         Map<String, String> x = new HashMap<>();
         x.put("baseURL", baseURI);
