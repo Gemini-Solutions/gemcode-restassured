5d6f6def3faa740f63a65298555b7bf3aea22097
Gem-Dipanshu-Kapoor <dipanshu.kapoor@geminisolutions.com>
2024-02-19 14:01:26 +0530

    compile time exception handling

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,9 @@ import java.util.Map;
 public class App {
     public Map<String, String> demo() {
         String baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
-        RestAssured.baseURI = baseURI;
+        RestAssured.baseURI = baseURI
         Response response = RestAssured.given().get().then().extract().response();
-        response.prettyPrint()
+        response.prettyPrint();
 
         //     return [base_url, method, params, headers, request body, response_body, status code]
         Map<String, String> x = new HashMap<>();
