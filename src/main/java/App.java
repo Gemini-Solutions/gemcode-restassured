5b8451a30d08ffd4a3e4a113a0d07e84d156adda
Dipanshu Kapoor <103283697+Gem-Dipanshu-Kapoor@users.noreply.github.com>
2024-02-19 14:11:14 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,8 +7,8 @@ import java.util.Map;
 public class App {
     public Map<String, String> demo() {
         String baseURI = "https://betaapi.gemecosystem.com/gemEcosystemDashboard/actuator/health";
-        RestAssured.baseURI = baseURI
-        Response response = RestAssured.given().get().then().extract().response();
+        RestAssured.baseURI = baseURI;
+        Response response = RestAssured.given().get().then().extract().response()
         response.prettyPrint();
 
         //     return [base_url, method, params, headers, request body, response_body, status code]
